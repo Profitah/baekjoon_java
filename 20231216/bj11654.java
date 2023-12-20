7e0444741e07234f6d299481d2b3e0668844de24
profitah <qwer120542@gmail.com>
2023-12-20 19:56:27 +0900

    9987 백준

--- 20231216/bj11654.java
+++ 20231216/bj11654.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // System.out.print(); // 이 부분을 삭제하거나 내용을 추가
-
+        // System.out.print(); 
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
         System.out.println((int) ch);
