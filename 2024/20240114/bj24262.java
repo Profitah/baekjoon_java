83e81458cda91bc4fb1a8cca38c482e4ab0108df
profitah <qwer120542@gmail.com>
2024-01-14 08:48:21 +0900

    24262 알고리즘의 수행시간 - 빅오 표기법

---
+++ 2024/20240114/bj24262.java
@@ -0,0 +1,8 @@
+// 빅오 표기법은 입력 크기에 관계 없이 동일한 시간이 걸린다. 즉 입력 크기가 증가하더라도 실행시간에 영향을 미치지 않는다. 
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
